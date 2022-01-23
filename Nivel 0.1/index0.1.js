4008fb94f74f02f18ffc45b7f000f6f6a103ad68
Alexandra Sivira <kerensivira@gmail.com>
2022-01-23 15:23:35 -0500

    j

--- Nivel 0.1/index0.1.js
+++ Nivel 0.1/index0.1.js
@@ -2,26 +2,17 @@ console.log("hola")
 
 let C = 30
 let resultado = Convertir (C);
-alert ((C)`°C`= (resultado)`°F`
-(F)`°F`= (resultado)`°C`);
+let F = (C * 1.8) +32;
 
-let F = resultado;
-let resultado = Convertir (F);
-
-
-function Convertir(C){
-
-let resultado = C * 1.8 + 32;
-
-return resultado; 
-
-}
 
 function Convertir(F){
 
-    let resultado = F * 1.8 - 32;
+let resultado = (C * 1.8) + 32;
+
+alert (resultado);
 
 }
+
 
 
 
