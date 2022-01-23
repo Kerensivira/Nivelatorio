3931a5a12339c8236f7142b262e966af51f96dfe
Alexandra Sivira <kerensivira@gmail.com>
2022-01-23 15:01:03 -0500

    j

---
+++ Nivel 0.1/index0.1.js
@@ -0,0 +1,28 @@
+console.log("hola")
+
+let C = 30
+let resultado = Convertir (C);
+alert ((C)`°C`= (resultado)`°F`
+(F)`°F`= (resultado)`°C`);
+
+let F = resultado;
+let resultado = Convertir (F);
+
+
+function Convertir(C){
+
+let resultado = C * 1.8 + 32;
+
+return resultado; 
+
+}
+
+function Convertir(F){
+
+    let resultado = F * 1.8 - 32;
+
+}
+
+
+
+
--- Nivel 0/index.js
+++
@@ -1,9 +0,0 @@
-var resultado = 86
-var C = 30
-
-    if ((C*1.8) +32) resultado; {
-    
-}
-
-alert ("30°C es igual a 86°F")
-
