db34edd231c271509f6e26733f824644356a28c9
Alexandra Sivira <kerensivira@gmail.com>
2022-01-25 19:54:16 -0500

    h

--- Nivel 0.2/index0.2.js
+++ Nivel 0.2/index0.2.js
@@ -1,6 +1,17 @@
-function saludar () {
+// function saludar () {
 
-    console.log("Hola soy una funcion");
+//     console.log("Hola soy una funcion");
+// }
+// saludar ()
+
+
+function obtenerDatoYSumar() {
+
+    var A = 10;
+    var number = document.getElementById('Seleccione monto').value;
+    var resultado = (A + number);
+
+    console.log(resultado);
 }
-saludar ()
 
+obtenerDatoYSumar();
