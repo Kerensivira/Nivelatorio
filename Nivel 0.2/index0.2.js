1f0d3981eb5fef3985c11f6ba14f4c844eb89e85
Alexandra Sivira <kerensivira@gmail.com>
2022-01-25 20:39:47 -0500

    h

--- Nivel 0.2/index0.2.js
+++ Nivel 0.2/index0.2.js
@@ -1,5 +1,5 @@
 // ------Nota para el tutor:
-//  Utilizo la función "saludar" para poder entender un poco mejor el
+//  Utilizo la función "saludar" para poder entender un poco mejor 
 // el concepto y uso de una función (conceptos y ejemplo obtenido del área de recursos del Git Kraken), 
 //era un concepto que no me había quedado del todo claro. En cuestión de los eventos aún me siento un poco falla, 
 //deseo trabajarlo un poco más.
